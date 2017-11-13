038ff0b62b93e8f3780746335f453ffd7a713676
Candace <crallison96@gmail.com>
2017-11-13 17:28:21 -0500

    Basic files created

---
+++ environment.cpp
@@ -0,0 +1,23 @@
+/** 
+ * Environment Class
+ * Candace Allison
+ *
+ Environmental Factors
+|-Water Supply
+| |-Available Water Locations ( less means higher danger )
+| |-Water Refill Speed ( less means lower supportable population )
+|-Disease
+| |-Variant of mutation ( Only affects certain species, encourages evolution )
+|-Space
+  |- Current Population ( Lower means more herbivores, Higher means more predators )
+  |-Foliage - Grass, Sandy, etc 
+ */
+
+class Environment
+{
+private:
+	
+
+public:
+
+}
