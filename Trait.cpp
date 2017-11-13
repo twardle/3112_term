99e4e215a5bd1de7e1c6aff280c7898df1860714
Tyler Wardle <twardle@uncc.edu>
2017-11-13 11:58:47 -0500

    patch

---
+++ Trait.cpp
@@ -0,0 +1,18 @@
+/*
+ * Trait.cpp
+ *
+ *  Created on: Nov 13, 2017
+ *      Author: Tyler
+ */
+
+#include "Trait.h"
+
+Trait::Trait() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Trait::~Trait() {
+	// TODO Auto-generated destructor stub
+}
+
