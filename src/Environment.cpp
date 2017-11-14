2037c2128bcacb79350ed32f22111a3694b83700
Tyler Wardle <twardle@uncc.edu>
2017-11-13 22:33:18 -0500

    initial patch

---
+++ src/Environment.cpp
@@ -0,0 +1,33 @@
+/*
+ * Environment.cpp
+ *
+ *  Created on: Nov 6, 2017
+ *      Author: Tyler
+ */
+
+#include "Environment.h"
+
+/*
+ * Environmental Factors
+ * |-Water Supply
+ * | |-Available Water Locations ( less means higher danger )
+ * | |-Water Refill Speed ( less means lower supportable population )
+ * |-Weather
+ * | |-Current Weather ( varies seasonally, encourages varied traits )
+ * | |-Temperature ( varies seasonally, encourages varied traits )
+ * |-Disease
+ * | |-Variant of mutation ( Only effects certain species, encourages evolution )
+ * |-Food Availability
+ *   |- Space ( Lower means more herbivores, Higher means more predators )
+ */
+
+
+Environment::Environment() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Environment::~Environment() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Omnivore.cpp
@@ -0,0 +1,28 @@
+/*
+ * Omnivore.cpp
+ *
+ *  Created on: Nov 6, 2017
+ *      Author: Tyler
+ */
+
+#include "Omnivore.h"
+
+/*
+ * Omnivore Factors
+ * |-Food
+ * | |-Optional Prey Supply
+ * |-Water Intake
+ * | |-Highest
+ * |-Breeding
+ *   |- Average
+ */
+
+Omnivore::Omnivore() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Omnivore::~Omnivore() {
+	// TODO Auto-generated destructor stub
+}
+
