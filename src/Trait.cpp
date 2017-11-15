3da19abab1d3be00ae145a8216ed84b0f533f296
Tyler Wardle <twardle@uncc.edu>
2017-11-15 11:13:42 -0500

    11/15/17 | 11:13am

--- src/Trait.cpp
+++ src/Trait.cpp
@@ -8,12 +8,40 @@
 #include "Trait.h"
 
 Trait::Trait() {
-	// TODO Auto-generated constructor stub
-
+	trait_name = "empty";
+	temp_resist = 1.00;
+	disease_resist = 1.00;
+	predator_resist = 1.00;
+	breedChance = 1.00;
+	herdTendency = 1.00;
+	waterNeed = 1.00;
 }
 
 Trait::~Trait() {
 	// TODO Auto-generated destructor stub
+}
+
+void setTempResist(float tr){
+	temp_resist = tr;
+}
+void setDiseaseResist(float dr){
+	disease_resist = dr;
+}
+
+void setPredatorResist(float pr){
+	predator_resist = pr;
+}
+
+void setBreedChance(float bc){
+	breedChance = bc;
+}
+
+void setHerdTendency(float ht){
+	herdTendency = ht;
+}
+
+void setWaterNeed(float wn){
+	waterNeed = wn;
 }
 
 bool Trait::operator==(const Trait& other) const {
